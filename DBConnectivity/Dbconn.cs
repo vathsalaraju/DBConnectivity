2a49335ef67105fc9c13c57aee7025e1c6a27a99
vathsalaraju <92009074+vathsalaraju@users.noreply.github.com>
2022-03-22 19:40:38 +0530

    Update Dbconn.cs

--- DBConnectivity/Dbconn.cs
+++ DBConnectivity/Dbconn.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +9,20 @@ using System.Configuration;
 
 namespace DBConnectivity
 {
-    public class InMemoryAppEngine// : AppEngine
+    public class InMemoryAppEngine : AppEngine
 
     {
-        List<Course> listOfCourses = new List<Course>();
-        List<Enroll> listOfEnrollment = new List<Enroll>();
-        List<Student> listOfStudents = new List<Student>();
+      
+        public List<Student> studentList = new List<Student>();
+        public List<Course> courseList = new List<Course>();
+        public List<Enroll> enrollList = new List<Enroll>();
         Enroll e = new Enroll();
         static int count = 0;
 
         public InMemoryAppEngine()
         {
         }
-        
-        #region Enroll Details
-        public void enroll(Student student, Course course, DateTime enrollmentDate)
+        public void enrollCourse(Student student, Course course, DateTime enrollmentDate)
         {
             Enroll e = new Enroll(student, course, enrollmentDate);
             int result = 0;
@@ -58,7 +57,7 @@ namespace DBConnectivity
                 return;
                 
             }
-            e.enrollList.Add(e);
+            enrollList.Add(e);
         }
 
         public void checkNumberOfEnrollments(string id)
@@ -84,6 +83,7 @@ namespace DBConnectivity
             catch(LimitRegistrationException e)
             {
                 Console.WriteLine(e.Message);
+                
             }
         }
 
@@ -114,9 +114,7 @@ namespace DBConnectivity
             }
 
         }
-    #endregion
-        
-        #region Student 
+    
         public void register(Student student)
         {
             try
@@ -149,9 +147,114 @@ namespace DBConnectivity
             
 
         }
-        #endregion
+        public List<Course> listOfCourses()
+        {
 
-        #region Course    
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connectionstr"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("select * from course", conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Course c;
+
+                            if ((bool)reader["isDegree"])
+                            {
+                                DegreeCourse dc = new DegreeCourse();
+                                dc.Id = (string)reader["id"];
+                                dc.Name = (string)reader["name"];
+                                dc.SeatsAvailable = (int)reader["seatsAvailable"];
+                                dc.Fee = (float)(decimal)reader["fee"];
+                                dc.Duration = (string)reader["Duration"];
+                                if ((string)reader["degreelevel"] == "BACHELORS")
+                                    dc.level = DegreeCourse.Level.BACHELORS;
+                                else
+                                    dc.level = DegreeCourse.Level.MASTERS;
+                                dc.isPlacementAvailable = (bool)reader["isPlacementAvailable"];
+                                int index = courseList.FindIndex(c => c.Id == dc.Id);
+                                if (index < 0)
+                                    courseList.Add(dc);
+                            }
+                            else
+                            {
+                                DiplomaCourse dpc = new DiplomaCourse();
+                                dpc.Id = (string)reader["id"];
+                                dpc.Name = (string)reader["name"];
+                                dpc.SeatsAvailable = (int)reader["seatsAvailable"];
+                                dpc.Fee = (float)(decimal)reader["fee"];
+                                dpc.Duration = (string)reader["Duration"];
+                                if ((string)reader["DiplomaType"] == "PROFESSIONAL")
+                                    dpc.type = DiplomaCourse.Type.PROFESSIONAL;
+                                else
+                                    dpc.type = DiplomaCourse.Type.ACADEMIC;
+                                int index = courseList.FindIndex(c => c.Id == dpc.Id);
+                                if (index < 0)
+                                    courseList.Add(dpc);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return courseList;
+        }
+        public List<Student> listOfStudents()
+        {
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connectionstr"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("select * from student", conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            Student student = new Student();
+                            student.Id = (string)reader["id"];
+                            student.Name = (string)reader["name"];
+                            student.Date = reader["DOB"].ToString();
+                            int index = studentList.FindIndex(s => s.Id == student.Id);
+                            if (index < 0)
+                                studentList.Add(student);
+                        }
+                    }
+                }
+            }
+            return studentList;
+        }
+        public List<Enroll> listOfEnrollments()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connectionstr"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("select * from enrollcourse", conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Enroll e1 = new Enroll();
+                            string sid = (string)reader["sid"];
+                            string cid = (string)reader["cid"];
+                            e1.EnrollmentDate = reader["enrollmentdate"].ToString();
+                            e1.Student = getStudentByid(sid);
+                            e1.Course = getCourseByid(cid);
+                            int index = enrollList.FindIndex(e => e.Student.Id == sid && e.Course.Id == cid);
+                            if (index < 0)
+                                enrollList.Add(e1);
+                        }
+                    }
+
+                }
+            }
+            return enrollList;
+        }
+
         public void introduce(Course course)
         {
             SqlCommand cmd;
@@ -225,8 +328,25 @@ namespace DBConnectivity
             
 
         }
-        #endregion
-                        
+        public Student getStudentByid(string id)
+        {
+            foreach (Student s in listOfStudents())
+            {
+                if (s.Id == id)
+                    return s;
+            }
+            throw new Exception("student id not found");
+        }
+        public Course getCourseByid(string id)
+        {
+            foreach (Course c in listOfCourses())
+            {
+                if (c.Id == id)
+                    return c;
+            }
+            throw new Exception("course id not found");
+        }
+
     }
 }
 
