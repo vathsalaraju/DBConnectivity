f07f7da9244cb6ab5b6a392c914b1e6c2c22c6a2
vathsalaraju <92009074+vathsalaraju@users.noreply.github.com>
2022-03-24 17:21:31 +0530

    Update Info.cs

--- DBConnectivity/Info.cs
+++ DBConnectivity/Info.cs
@@ -10,7 +10,7 @@ namespace DBConnectivity
     {
         public static void Display(Student student)
         {
-            Console.WriteLine("Student ID : "+student.Id + "\tStudentName : " + student.Name + "\tDate of Birth : " + student.Date);
+            Console.WriteLine("Student ID : "+student.Id + "\tStudentName : " + student.Name + "\tDate of Birth : " + student.Date.ToShortDateString());
         }
 
         public void Display(Course course)
@@ -20,7 +20,7 @@ namespace DBConnectivity
 
         public void Display(Enroll enroll)
         {
-            Console.WriteLine("Student ID : "+enroll.Student.Id+"\tStudent Name : "+enroll.Student.Name+"\tCourse ID : "+enroll.Course.Id+"\t Course Name : "+enroll.Course.Name+"\tDuration : "+enroll.Course.Duration+"\tEnrollment Date : "+enroll.EnrollmentDate);
+            Console.WriteLine("Student ID : "+enroll.Student.Id+"\tStudent Name : "+enroll.Student.Name+"\tCourse ID : "+enroll.Course.Id+"\t Course Name : "+enroll.Course.Name+"\tDuration : "+enroll.Course.Duration+"\tEnrollment Date : "+enroll.EnrollmentDate.ToShortDateString());
         }
 
     }
